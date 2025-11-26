a8f24a3b8f8572c333bb886e8fbe4b0eea05dd30
akthernahela <nahela00@gmail.com>
2025-11-26 18:05:18 +0100

    I notice that i don't have the patch root

--- posts.js
+++ posts.js
@@ -2,22 +2,27 @@ const express = require('express');
 const router = express.Router();
 const products = require('./data/posts')
 
+//index
 router.get('/', (req, res) => {
   res.json(posts);
 });
 
+//show
 router.get('/:id', (req, res) => {
   res.send(`Singolo prodotto ${req.params.id}`);
 });
 
+//store
 router.post('/', (req, res) => {
   res.send('Aggiungi un nuovo prodotto');
 });
 
+//update
 router.put('/:id', (req, res) => {
   res.send(`Aggiorna il prodotto ${req.params.id}`);
 });
 
+//destroy
 router.delete('/:id', (req, res) => {
   res.send(`Cancella un prodotto ${req.params.id}`);
 });
