8b8c8642e4137006e2706f645c433b54bc5b5a59
akthernahela <nahela00@gmail.com>
2025-11-26 19:08:30 +0100

    Fix the index root ps: later*

--- controllers/prodController.js
+++ controllers/prodController.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 //index
 const index = (req, res) => {
-    let selectProduct = products;
-    /*if (req.query.tags) {
+    /*let selectProduct = products;
+    if (req.query.tags) {
         selectProduct = products.filter(
             product => product.tags.includes(req.query.tags)
         );
