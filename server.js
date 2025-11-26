0670c85117ef94043455ee2397f3ad60644a513b
akthernahela <nahela00@gmail.com>
2025-11-26 11:02:03 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+const productsRouter = require('./posts')
+
+app.use(express.static('public'));
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send(`Server del mio blog`);
+});
+
+app.use('/posts', productsRouter);
+
+/*
+app.get('/posts', (req, res) => {
+    res.send(`Lista dei prodotti`);
+});
+
+app.get('/posts/:id', (req, res) => {
+    res.send(`Singolo prodotto ${req.params.id}`);
+});
+
+app.post('/posts', (req, res) => {
+    res.send('Aggiungi un nuovo prodotto');
+});
+
+app.put('/posts/:id', (req, res) => {
+    res.send(`Aggiorna il prodotto ${req.params.id}`);
+});
+
+app.delete('/posts/:id', (req, res) => {
+    res.send(`Cancella un prodotto ${req.params.id}`);
+});
+*/
