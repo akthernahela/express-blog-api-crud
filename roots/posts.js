24d12ea1a58586b75fa2cbac0b6d18ad301d90b2
akthernahela <nahela00@gmail.com>
2025-11-28 11:25:29 +0100

    Change the roots and add a file

--- roots/posts.js
+++ roots/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const prodController = require('./controllers/prodController');
+const prodController = require('../controllers/prodController');
 
 
 //index
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
-const productsRouter = require('./posts')
+const productsRouter = require('./roots/posts')
 
 app.use(express.static('public'));
 app.listen(PORT, () => {
